11605eb5908343ff484cb4e258d53999b0694ad2
Arteeemiy <artemarisius1@gmail.com>
2024-12-21 14:16:20 +0300

    Add files via upload

---
+++ src/test.py
@@ -0,0 +1,21 @@
+﻿from utils import create_model, predict_image
+import torch
+import os
+
+if __name__ == "__main__":
+    model_path = "models/resnet50.pth"
+    image_path = input("Введите путь до подававаемого изображения: ")
+
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(f"Model file not found: {model_path}")
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"Image file not found: {image_path}")
+
+    model = create_model()
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
+
+    class_names = ["Fracture", "Not Fracture"]
+
+    predicted_class = predict_image(model, image_path, class_names)
+
+    print(f"The predicted class is: {predicted_class}")
